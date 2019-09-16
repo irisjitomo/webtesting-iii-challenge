7253b0be92d70a39cd310619f60eb9ec5f597a28
irisjitomo <irisjitomo@eyahoo.com>
2019-09-16 18:45:40 -0400

    completed halfway

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
 // Test away
+import React from 'react';
+import { render, Component } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+import Display from '../display/Display'
+import Controls from '../controls/Controls'
+
+test('renders Dashboard', () => {
+	render(<Dashboard />);
+});
+
+// const component = <Dashboard />
+
+test('shows display', () => {
+        expect(render(<Display />)).toMatchSnapshot();
+		// expect(component.contains(<Display />)).toBe(true);
+});
+
+test('shows controls', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
+});
